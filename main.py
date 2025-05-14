229322c8f8b8ec2683bb8bc97888f7f2cca40fcf
jolieyolie <congyi.h.f@gmail.com>
2025-05-14 17:40:47 +0200

    C:

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import pygame
+
+from constants import *
+
 def main():
+    # initialize the pygame module
+    pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #print(screen)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color = (0, 0, 0), rect=None, special_flags=0)
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
